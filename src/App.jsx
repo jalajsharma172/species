f3d3521542d2be847e54e1e28d6c2dc60fc7837e
jalajsharma172 <jalajsharma172@gmail.com>
2026-05-15 00:02:37 +0530

    framework changed to react

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useRef, useEffect } from 'react'
+import Layout from './components/Layout'
+import Hero from './components/Hero'
+import RawShowcase from './components/RawShowcase'
+import PackagingSection from './components/PackagingSection'
+import FinalReveal from './components/FinalReveal'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+export default function App() {
+  const particleRef = useRef(null)
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+  }, [])
+
+  return (
+    <Layout particleRef={particleRef}>
+      <main className="relative">
+        <Hero particleRef={particleRef} />
+        <RawShowcase particleRef={particleRef} />
+        <PackagingSection particleRef={particleRef} />
+        <FinalReveal particleRef={particleRef} />
+      </main>
+    </Layout>
+  )
+}
